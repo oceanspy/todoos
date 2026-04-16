9ae8650116c3dba086adc09d54c237ec1a0d3395
Cedric BULTEEL <cedric@oceanspy.com>
2026-04-16 22:15:55 +0200

    Improving help

--- src/Help/HelpPrinter.cpp
+++ src/Help/HelpPrinter.cpp
@@ -9,8 +9,7 @@ HelpPrinter::HelpPrinter(IOService& ioService)
 void
 HelpPrinter::show()
 {
-    ioService.message("Help Center");
-    ioService.message("----");
+    ioService.message(" Help Center");
     for (std::string help : helpListForCli()) {
         ioService.message(help);
     }
@@ -60,95 +59,93 @@ HelpPrinter::commandOptionNotSupportedSkipCommandAutocomplete(Command& command)
 std::vector<std::string>
 HelpPrinter::helpListForCli()
 {
-    return {
-        "\033[96m-h/--help\033[0m                                                       Show the help center",
-        "\033[96m-v/--version\033[0m                                                    Show current version of the "
-        "app",
-        "\033[96m-c/--config\033[0m                                                     Show the config of the app",
-        "\033[96m-a/--archive\033[0m                                                    Show the archived items (valid "
-        "for show/find command)",
-        "\033[96m-d/--delete\033[0m                                                     Show the deleted items (valid "
-        "for show/find command)",
-        "\033[96m-f/--force\033[0m                                                      Force an action (valid for "
-        "delete, copy-to & move-to commands)",
-        "\033[96m-Q/--no-quit\033[0m                                                    Force the application to not "
-        "quit at the end of the process",
-        "",
-        "\033[94m-l/--list {name}\033[0m                                                Use a list for the command",
-        "\033[94m-p/--priority {name}\033[0m                                            Precise the priority of the "
-        "item(s)",
-        "\033[94m-s/--status {name}\033[0m                                              Precise the status of the "
-        "item(s)",
-        "\033[94m-i/--deadline {cf. deadline}\033[0m                                    Precise the deadline of the "
-        "item",
-        "",
-        "\033[0muse \033[93m{list}\033[0m                                                      Use a list",
-        "\033[0mlist\033[0m                                                            Show all available lists",
-        "     \033[0madd \033[93m{name}\033[0m                                                 Create new list",
-        "     \033[0mrm \033[93m{name}\033[0m                                                  Remove list",
-        "     \033[0mmv \033[93m{name} {new_nam}\033[0m                                        Remove list",
-        "     \033[0mcurrent\033[0m                                                    Show the current list",
-        "     \033[0mcopy \033[93m{listName} {newListName}\033[0m                              Copy/Duplicate the list",
-        "",
-        "\033[0mshow () \033[94m[-p {p}] [-s {S}] [-i {I}] \033[96m[-a] [-d]\033[0m                    Show items of "
-        "the list",
-        "\033[0madd (a) \033[93m{value} \033[94m[-p {p}] [-s {S}] [-i {I}]\033[0m                      Add a new item "
-        "to the list",
-        "\033[0medit (e) \033[93m{id} {value} \033[94m[-p {p}] [-s {S}] [-i {I}]\033[0m                Edit an item of "
-        "the list",
-        "\033[0mappend \033[93m{id} {value} \033[94m[-p {p}] [-s {S}] [-i {I}]\033[0m                  Append text to "
-        "an item of the list",
-        "\033[0mprepend \033[93m{id} {value} \033[94m[-p {p}] [-s {S}] [-i {I}]\033[0m                 Prepend text to "
-        "an item of the list",
-        "",
-        "\033[0mfind (f) \033[93m{word} \033[96m[-a] [-d]\033[0m                                       Find an item in "
-        "the list",
-        "",
-        "\033[0mremove (rm) \033[96m[-f] \033[93m{ids}\033[0m                                          Remove an item "
-        "from the list",
-        "\033[0mmove-to (mv) \033[93m{newList} {ids}\033[0m                                    Move an item to another "
-        "list",
-        "\033[0mcopy-to (cp) \033[93m{newList} {ids}\033[0m                                    Copy an item to another "
-        "list",
-        "\033[0mduplicate \033[93m{ids}\033[0m                                                 Duplicate an item on "
-        "the list",
-        "\033[0marchive \033[93m{ids}\033[0m                                                   Move the item(s) to the "
-        "archive list",
-        "\033[0mrestore \033[93m{ids}\033[0m                                                   Restore "
-        "archived/deleted item(s)",
-        "",
-        "\033[0mpriority (p) \033[93m{priority} {ids}\033[0m                                   Set priority to one or "
-        "more items",
-        "\033[0mincrease (inc)/decrease (dec) \033[93m{1->4} {ids}\033[0m                      Increase or decrease "
-        "priority by {1->4}(optional) amount to one or more items",
-        "\033[0mdeadline \033[93m{date} {ids}\033[0m                                           Add a deadline to an "
-        "item",
-        "     \033[93m{date} \033[90m> Format: YYYY-MM-DD, MM-DD, DD\033[0m",
-        "     \033[93m{date} \033[90m> Format: today, tomorrow, next-week, next-month, next-year\033[0m",
-        "     \033[93m{date} \033[90m> Format: 1d, 1w, 1m, 1y, xday(s), xweek(s), xmonth(s), xyear(s)\033[0m",
-        "     \033[93m{date} \033[90m> Remove: reset/none\033[0m",
-        "",
-        "\033[0mreset \033[93m{ids}/[all]\033[0m                                               Move the item(s)/all "
-        "items to 'To-do' & reset timings",
-        "\033[0mto-do \033[93m{ids}\033[0m                                                     Set the item(s) status "
-        "to 'To do'",
-        "\033[0mstart \033[93m{ids}\033[0m                                                     Set the item(s) status "
-        "to 'started'",
-        "\033[0mpause \033[93m{ids}\033[0m                                                     Set the item(s) status "
-        "to 'paused'",
-        "\033[0mreview \033[93m{ids}\033[0m                                                    Set the item(s) status "
-        "to 'review'",
-        "\033[0mpending \033[93m{ids}\033[0m                                                   Set the item(s) status "
-        "to 'pending'",
-        "\033[0mcomplete \033[93m{ids}\033[0m                                                  Set the item(s) status "
-        "to 'completed'",
-        "\033[0mcancel \033[93m{ids}\033[0m                                                    Set the item(s) status "
-        "to 'cancel'",
-        "",
-        "\033[0mclean\033[0m                                                           Archive all Done/Cancelled of "
-        "the list",
-        "\033[0mempty\033[0m                                                           Empty the list",
+    // Colors
+    const std::string RST  = "\033[0m";
+    const std::string BOLD = "\033[1m";
+    const std::string CYN  = "\033[96m";   // flags
+    const std::string BLU  = "\033[94m";   // options
+    const std::string YLW  = "\033[93m";   // arguments
+    const std::string DIM  = "\033[90m";   // dim / hints
+    const std::string SEP  = DIM + " \xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80\xE2\x94\x80" + RST;
+
+    auto section = [&](const std::string& title) {
+        return BOLD + " " + title + RST;
     };
+
+    // clang-format off
+    return {
+        SEP,
+        "",
+        section("FLAGS"),
+        "   " + CYN + "-h/--help"    + RST + "         Show the help center",
+        "   " + CYN + "-v/--version" + RST + "      Show app version",
+        "   " + CYN + "-c/--config"  + RST + "       Show app config",
+        "   " + CYN + "-a/--archive" + RST + "      Include archived items  " + DIM + "(show, find)" + RST,
+        "   " + CYN + "-d/--delete"  + RST + "       Include deleted items   " + DIM + "(show, find)" + RST,
+        "   " + CYN + "-f/--force"   + RST + "        Force action            " + DIM + "(remove, copy-to, move-to)" + RST,
+        "   " + CYN + "-Q/--no-quit" + RST + "      Keep app alive after command",
+        "",
+        section("OPTIONS"),
+        "   " + BLU + "-l/--list {name}"     + RST + "         Target list",
+        "   " + BLU + "-p/--priority {name}" + RST + "     Filter/set priority",
+        "   " + BLU + "-s/--status {name}"   + RST + "       Filter/set status",
+        "   " + BLU + "-i/--deadline {date}" + RST + "     Filter/set deadline",
+        "",
+        section("LISTS"),
+        "   use "  + YLW + "{list}"           + RST + "                   Switch active list",   // col 13 + 19 = 32
+        "   list                         Show all lists",                                         // col 7  + 25 = 32
+        "     add " + YLW + "{name}"          + RST + "                 Create list",            // col 15 + 17 = 32
+        "     rm  " + YLW + "{name}"          + RST + "                 Remove list",            // col 15 + 17 = 32
+        "     mv  " + YLW + "{name} {new_name}" + RST + "      Rename list",                    // col 26 +  6 = 32
+        "     current                    Show active list",                                       // col 12 + 20 = 32
+        "     copy " + YLW + "{name} {new_name}" + RST + "     Duplicate list",                 // col 27 +  5 = 32
+        "",
+        section("ITEMS"),
+        "   show                         Show items     " + BLU + "[-p] [-s] [-i] " + CYN + "[-a] [-d]" + RST, // col 7  + 25 = 32
+        "   add     (a) " + YLW + "{value}"      + RST + "          Add item     " + BLU + "[-p] [-s] [-i]" + RST, // col 22 + 10 = 32
+        "   edit    (e) " + YLW + "{id} {value}" + RST + "     Edit item     " + BLU + "[-p] [-s] [-i]" + RST,    // col 27 +  5 = 32
+        "   append      " + YLW + "{id} {value}" + RST + "     Append to item" + BLU + " [-p] [-s] [-i]" + RST,  // col 27 +  5 = 32
+        "   prepend     " + YLW + "{id} {value}" + RST + "     Prepend to item" + BLU + " [-p] [-s] [-i]" + RST, // col 27 +  5 = 32
+        "   find    (f) " + YLW + "{word}"       + RST + "           Find item              " + CYN + "[-a] [-d]" + RST, // col 21 + 11 = 32
+        "",
+        section("MOVE & COPY"),
+        "   remove  (rm) " + CYN + "[-f] " + YLW + "{ids}" + RST + "       Remove items",              // col 26 +  7 = 33
+        "   move-to (mv) " + YLW + "{list} {ids}"           + RST + "     Move to list",               // col 28 +  5 = 33
+        "   copy-to (cp) " + YLW + "{list} {ids}"           + RST + "     Copy to list",               // col 28 +  5 = 33
+        "   duplicate    " + YLW + "{ids}"                  + RST + "            Duplicate items",      // col 21 + 12 = 33
+        "   archive      " + YLW + "{ids}"                  + RST + "            Send to archive",      // col 21 + 12 = 33
+        "   restore      " + YLW + "{ids}"                  + RST + "            Restore archived/deleted items", // col 21 + 12 = 33
+        "",
+        section("PRIORITY"),
+        "   priority  (p)   " + YLW + "{priority} {ids}" + RST + "     Set priority",   // col 35 + 5 = 40
+        "   increase  (inc) " + YLW + "[1-4] {ids}"     + RST + "          Raise priority", // col 30 + 10 = 40
+        "   decrease  (dec) " + YLW + "[1-4] {ids}"     + RST + "          Lower priority", // col 30 + 10 = 40
+        "",
+        section("DEADLINE"),
+        "   deadline " + YLW + "{date} {ids}"                     + RST + "                 Set deadline",
+        "     " + DIM + "YYYY-MM-DD \xC2\xB7 MM-DD \xC2\xB7 DD" + RST,
+        "     " + DIM + "today \xC2\xB7 tomorrow \xC2\xB7 next-week \xC2\xB7 next-month \xC2\xB7 next-year" + RST,
+        "     " + DIM + "1d \xC2\xB7 1w \xC2\xB7 1m \xC2\xB7 1y \xC2\xB7 Xday(s) \xC2\xB7 Xweek(s) \xC2\xB7 Xmonth(s) \xC2\xB7 Xyear(s)" + RST,
+        "     " + DIM + "remove: reset / none" + RST,
+        "",
+        section("STATUS"),
+        "   to-do    " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 To do",          // col 17 + 7 = 24
+        "   start    " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Started",
+        "   pause    " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Paused",
+        "   review   " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Review",
+        "   pending  " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Pending",
+        "   complete " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Completed",
+        "   cancel   " + YLW + "{ids}"     + RST + "       \xE2\x86\x92 Cancelled",
+        "   reset    " + YLW + "{ids/all}" + RST + "   Reset to 'To do' & clear timings", // col 21 + 3 = 24
+        "",
+        section("BULK"),
+        "   clean              Archive all completed/cancelled items",
+        "   empty              Empty the list",
+        "",
+        SEP,
+        DIM + "   shortcuts: create \xC2\xB7 up/upper \xC2\xB7 low/lower \xC2\xB7 current \xC2\xB7 all" + RST,
+    };
+    // clang-format on
 }
 
 std::string
